178db5c80fc1948732c88b32b631a42bb657ead3
apavlov <a.pavlov@admitad.com>
2017-11-23 17:18:22 +0300

    Initial Commit

---
+++ SimpeScoreObjectiveC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpeScoreObjectiveC
+//
+//  Created by Anton Pavlov on 23.11.2017.
+//  Copyright © 2017 Anton Pavlov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
